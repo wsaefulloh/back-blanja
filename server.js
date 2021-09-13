d28a24f25c0cc84d7ffb9cd10a9ca49ef6acf695
Wahyu Saefulloh <saefuuloh@gmail.com>
2021-09-13 13:44:10 +0700

    CI/CD Jenkins

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ if (process.env.NODE_ENV === "prod") {
     dotenv.config({ path: './.env.production' })
 }
 if (process.env.NODE_ENV === "test") {
-    dotenv.config({ path: './.env.test' })
+    dotenv.config({ path: './.env' })
 }
 
 const {orm : database} = require('./src/configs/db');
